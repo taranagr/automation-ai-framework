8beca74eac1ffb5cf25950f9df41407b91c3e4b9
taranagr <taranagrawal@gmail.com>
2026-04-22 11:00:17 -0500

    Updated Main to call healenium_setup() and open_browser() functions

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # This is a sample Python script.
+import os
+
+from common_library.healenium_setup import healenium_setup
+from common_library.selenium_functions import open_browser
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -6,8 +11,12 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print(f'Hi, {name}')
+    # Press Ctrl+F8 to toggle the breakpoint.
+    os.environ["HEALENIUM_FLAG"] = "YES"
+    healenium_setup()
+    driver = open_browser()
+    driver.get("https://www.google.com")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
